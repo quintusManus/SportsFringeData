7e0ad621972388c78ac97753e9c2420787ca9bee
Benjamin Woods <benewoods02@gmail.com>
2022-07-06 14:32:00 -0400

    added a main menu

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sportsipy.ncaab.schedule import Schedule
 
 # prints an ncaa team's current schedule
 def ncaa_team_schedule():
-    team = input("")
+    team = input("Enter the NCAA team's name ")
     schedule = Schedule(team)
     for game in schedule:
         print(game.date)
@@ -18,9 +18,26 @@ def print_most_wins(year, wins):
     most_wins = max(wins, key=wins.get)
     print('%s: %s - %s' % (year, wins[most_wins], most_wins))
 
-
+'''
 for year in range(2000, 2019):
     wins = {}
     for team in Teams(year):
         wins[team.name] = team.wins
     print_most_wins(year, wins)
+'''
+
+end = 0
+while end == 0:
+    print("Type the number of the sport you want")
+    print("1. NCAA")
+    text = input("")
+
+    if text == '1':
+        print("Choose the number of your fringe question")
+        print("1. What's this year's schedule for BLANK team?")
+        text = input("")
+        if text == '1':
+            ncaa_team_schedule()
+            text = input("Want to exit? press 1 ")
+            if text == '1':
+                end = 1
