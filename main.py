bfa27c87aea69bf8965bd7d1f686f2aa4df9ebdc
Benjamin Woods <benewoods02@gmail.com>
2022-07-06 14:51:57 -0400

    update

--- main.py
+++ main.py
@@ -18,14 +18,6 @@ def print_most_wins(year, wins):
     most_wins = max(wins, key=wins.get)
     print('%s: %s - %s' % (year, wins[most_wins], most_wins))
 
-'''
-for year in range(2000, 2019):
-    wins = {}
-    for team in Teams(year):
-        wins[team.name] = team.wins
-    print_most_wins(year, wins)
-'''
-
 end = 0
 while end == 0:
     print("Type the number of the sport you want")
